760b005fd7ea01dfa4a3e525344e4f4b6bfb5a2c
chimingkuei <chimingkuei@gmail.com>
2026-01-17 00:30:42 +0800

    FTPClient Ver.1.0.0.0

---
+++ FTPClient/FTP.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPClient
+{
+    interface IData
+    {
+        void GetAllDirectory(DirectoryInfo dir, List<DirectoryInfo> list, bool system = false, bool hidden = false);
+    }
+
+    class FTP: IData
+    {
+        public string ftpUser { get; set; }
+        public string ftpPassword { get; set; }
+        public string ftpRootURL { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+
+        public FTP(string _ftpUser, string _ftpPassword, string _ip, string _port)
+        {
+            ftpUser = _ftpUser;
+            ftpPassword = _ftpPassword;
+            ip = _ip;
+            port = _port;
+            ftpRootURL = "ftp://" + ip + (string.IsNullOrEmpty(_port) ? "" : ":" + port);
+        }
+
+        public bool FileUpload(FileInfo localFile, string ftpPath, string ftpFileName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FileStream localFileStream = null;
+            Stream requestStream = null;
+            try
+            {
+                string uri = ftpRootURL + ftpPath + ftpFileName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(ftpUser, ftpPassword);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.KeepAlive = false;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpWebRequest.ContentLength = localFile.Length;
+                int buffLength = 2048;
+                byte[] buff = new byte[buffLength];
+                int contentLen;
+                localFileStream = localFile.OpenRead();
+                requestStream = ftpWebRequest.GetRequestStream();
+                contentLen = localFileStream.Read(buff, 0, buffLength);
+                while (contentLen != 0)
+                {
+                    requestStream.Write(buff, 0, contentLen);
+                    contentLen = localFileStream.Read(buff, 0, buffLength);
+                }
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (requestStream != null)
+                {
+                    requestStream.Close();
+                }
+                if (localFileStream != null)
+                {
+                    localFileStream.Close();
+                }
+            }
+            return success;
+        }
+
+        public bool FileDownload(string localPath, string localFileName, string ftpPath, string ftpFileName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FtpWebResponse ftpWebResponse = null;
+            Stream ftpResponseStream = null;
+            FileStream outputStream = null;
+            try
+            {
+                outputStream = new FileStream(localPath + localFileName, FileMode.Create);
+                string uri = ftpRootURL + ftpPath + ftpFileName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(ftpUser, ftpPassword);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                ftpWebResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                ftpResponseStream = ftpWebResponse.GetResponseStream();
+                long contentLength = ftpWebResponse.ContentLength;
+                int bufferSize = 2048;
+                byte[] buffer = new byte[bufferSize];
+                int readCount;
+                readCount = ftpResponseStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = ftpResponseStream.Read(buffer, 0, bufferSize);
+                }
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (outputStream != null)
+                {
+                    outputStream.Close();
+                }
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        public bool FileDelete(string ftpPath, string ftpName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FtpWebResponse ftpWebResponse = null;
+            Stream ftpResponseStream = null;
+            StreamReader streamReader = null;
+            try
+            {
+                string uri = ftpRootURL + ftpPath + ftpName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(ftpUser, ftpPassword);
+                ftpWebRequest.KeepAlive = false;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.DeleteFile;
+                ftpWebResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                long size = ftpWebResponse.ContentLength;
+                ftpResponseStream = ftpWebResponse.GetResponseStream();
+                streamReader = new StreamReader(ftpResponseStream);
+                string result = String.Empty;
+                result = streamReader.ReadToEnd();
+
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        // 若return false表示檔案不存在
+        public bool FileCheckExist(string ftpPath, string ftpName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            WebResponse webResponse = null;
+            StreamReader reader = null;
+            try
+            {
+                string url = ftpRootURL + ftpPath;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(url));
+                ftpWebRequest.Credentials = new NetworkCredential(ftpUser, ftpPassword);
+                ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                ftpWebRequest.KeepAlive = false;
+                webResponse = ftpWebRequest.GetResponse();
+                reader = new StreamReader(webResponse.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line == ftpName)
+                    {
+                        success = true;
+                        break;
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (webResponse != null)
+                {
+                    webResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        // 若return true表示資料夾不存在，則建立其資料夾
+        public bool FolderCheckExist(string ftpPath, string ftpDirName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            WebResponse webResponse = null;
+            Stream ftpResponseStream = null;
+            try
+            {
+                string url = ftpRootURL + ftpPath + ftpDirName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(url));
+                ftpWebRequest.Credentials = new NetworkCredential(ftpUser, ftpPassword);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+                FtpWebResponse response = (FtpWebResponse)ftpWebRequest.GetResponse();
+                ftpResponseStream = response.GetResponseStream();
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (webResponse != null)
+                {
+                    webResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        public void GetAllDirectory(DirectoryInfo dir, List<DirectoryInfo> list, bool system = false, bool hidden = false)
+        {
+            DirectoryInfo[] sub = dir.GetDirectories();
+            if (sub.Length > 0)
+            {
+                list.Add(dir);
+            }
+            else if (sub.Length == 0)
+            {
+                list.Add(dir);
+                return;
+            }
+            foreach (DirectoryInfo subDir in sub)
+            {
+                // 跳过系统目录
+                if (system && (subDir.Attributes & FileAttributes.System) == FileAttributes.System)
+                    continue;
+                // 跳过隐藏目录
+                if (hidden && (subDir.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    continue;
+                GetAllDirectory(subDir, list);
+            }
+        }
+
+        public IEnumerable<(string, string[], string[])> Walk(string rootPath)
+        {
+            // 遍歷當前目錄下的所有文件和子目錄         
+            string[] files = Directory.GetFiles(rootPath);
+            string[] subDirs = Directory.GetDirectories(rootPath);
+
+            yield return (rootPath, subDirs, files);
+
+            // 遞歸遍歷子目錄      
+            foreach (string subDir in subDirs)
+            {
+                foreach (var tuple in Walk(subDir))
+                {
+                    yield return tuple;
+                }
+            }
+        }
+
+
+    }
+
+
+}
---
+++ FTPClient/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTPClient
+{
+    public class Parameter
+    {
+        public string IP_val { get; set; }
+        public string Port_val { get; set; }
+        public string UserName_val { get; set; }
+        public string Password_val { get; set; }
+        public string Local_Path_val { get; set; }
+        public string Ftp_Path_val { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        public string TextBoxDispatcherGetValue(TextBox control)
+        {
+            string name = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                name = control.Text;
+            });
+            return name;
+        }
+
+        public void TextBoxDispatcherSetValue(string name, TextBox control)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                control.Text = name;
+            });
+        }
+
+        #region Config
+        private void LoadConfig()
+        {
+            List<Parameter> Parameter_info = Config.Load();
+            IP.Text = Parameter_info[0].IP_val;
+            Port.Text = Parameter_info[0].Port_val;
+            UserName.Text = Parameter_info[0].UserName_val;
+            Password.Text = Parameter_info[0].Password_val;
+            Local_Path.Text = Parameter_info[0].Local_Path_val;
+            Ftp_Path.Text = Parameter_info[0].Ftp_Path_val;
+        }
+
+        private void SaveConfig()
+        {
+            List<Parameter> Student_config = new List<Parameter>()
+                        {
+                            new Parameter() {IP_val = IP.Text,
+                                             Port_val = Port.Text,
+                                             UserName_val = UserName.Text,
+                                             Password_val=Password.Text,
+                                             Local_Path_val=Local_Path.Text,
+                                             Ftp_Path_val= Ftp_Path.Text
+                                             }
+                        };
+            Config.Save(Student_config);
+            Logger.WriteLog("儲存參數!", 1, richTextBoxGeneral);
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig();
+        }
+        BaseLogRecord Logger = new BaseLogRecord();
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(FileUpload):
+                    {
+                        string ftp_last_folder = System.IO.Path.GetFileName(Ftp_Path.Text);
+                        if (System.IO.Path.GetFileName(Local_Path.Text) == ftp_last_folder)
+                        {
+                            Task.Run(() =>
+                            {
+                                FTP Do = new FTP(TextBoxDispatcherGetValue(UserName), TextBoxDispatcherGetValue(Password), TextBoxDispatcherGetValue(IP), "23");
+                                string Ftp_Path_Invokval = TextBoxDispatcherGetValue(Ftp_Path);
+                                int upload_file_number = 0;
+                                foreach (var (dirPath, subDirs, files) in Do.Walk(TextBoxDispatcherGetValue(Local_Path)))
+                                {
+                                    Do.FolderCheckExist(Ftp_Path_Invokval, Regex.Split(dirPath, ftp_last_folder)[1].Replace("\\", "/"));
+                                    foreach (string filePath in files)
+                                    {
+                                        bool filestate = Do.FileCheckExist(Ftp_Path_Invokval, Regex.Split(filePath, ftp_last_folder)[1].Replace("\\", "/"));
+                                        if (!filestate)
+                                        {
+                                            Do.FileUpload(new FileInfo(filePath), Ftp_Path_Invokval, Regex.Split(filePath, ftp_last_folder)[1].Replace("\\", "/"));
+                                            upload_file_number++;
+                                            Console.WriteLine($"FTP建立{Regex.Split(filePath, ftp_last_folder)[1].Replace("\\", "/")}");
+                                        }
+                                    }
+                                }
+                                Console.WriteLine($"FTP總共上傳{upload_file_number}檔案!");
+                            });
+                        }
+                        else
+                        {
+                            MessageBox.Show("請確認本端與上傳ftp路徑最後一層資料夾名稱相同!", "警告", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+                case nameof(Open_Dir):
+                    {
+                        System.Windows.Forms.FolderBrowserDialog path = new System.Windows.Forms.FolderBrowserDialog();
+                        path.ShowDialog();
+                        Local_Path.Text = path.SelectedPath;
+                        break;
+                    }
+                case nameof(Save_Config):
+                    {
+                        SaveConfig();
+                        break;
+                    }
+
+            }
+        }
+        #endregion
+
+
+
+
+
+    }
+}
